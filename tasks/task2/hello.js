695c7a427bdb57cb8f51089fcc6c34c57aaff93a
Adrian6476 <adrian6476@outlook.com>
2024-07-17 16:58:22 +0100

    task2

---
+++ tasks/task2/hello.js
@@ -0,0 +1,17 @@
+function hello(name) {
+    return `hello, ${name}!`;
+}
+
+function bye(name) {
+    return `bye, ${name}!`;
+}
+
+function morning(name) {
+    return `morning, ${name}!`;
+}
+
+module.exports = {
+    hello,
+    bye,
+    morning
+};
---
+++ tasks/task2/main.js
@@ -0,0 +1,5 @@
+const { hello, bye, morning } = require('./hello');
+
+console.log(hello('world'));
+console.log(bye('lucy'));
+console.log(morning('ellen')); 
