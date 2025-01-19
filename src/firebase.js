f55f754f1e3766188541f777f9472fd21df7cd3b
Adrian6476 <adrian6476@outlook.com>
2025-01-19 14:13:13 +0800

    add Firebase configuration and cleanup script; update package name; add firebase secrets

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "REMOVED",
-    authDomain: "REMOVED",
-    projectId: "REMOVED",
-    storageBucket: "REMOVED.appspot.com",
-    messagingSenderId: "REMOVED",
-    appId: "1:REMOVED:web:fac562e7e318273dbdaf83",
-    measurementId: "REMOVED"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
